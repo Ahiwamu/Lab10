35b9702faec53c15c24519a45e4f30f25b6e5f3f
Nice <niceziio08@gmail.com>
2024-12-17 16:04:12 +0700

    finished

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+
+    string text;
+    double sum = 0.0;
+    double sumsqrt = 0.0; 
+    int count = 0;
+
+    while(getline(source,text)){
+        sum += stod(text);
+        sumsqrt += pow(stod(text),2);
+        count++;
+    }
+    
+    double mean = sum / count;
+    double sd = sqrt(double(sumsqrt/count) - pow(mean,2));
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd;
+}
